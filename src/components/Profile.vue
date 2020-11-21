e537e8684aba6da10d24c94513eebb6f42c5e06c
Nikki <piknik@me.com>
2020-11-21 08:44:13 -0700

    add components

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -6,7 +6,7 @@
 
 <script>
 export default {
-  name: 'Map'
+  name: 'Profile'
 }
 </script>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,34 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Home from '../components/Home'
+import Profile from '../components/Profile'
+import Signup from '../components/Signup'
+import Login from '../components/Login'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/profile/:slug',
+      name: 'Profile',
+      component: Profile
+    },
+    {
+      path: '/signup',
+      name: 'Signup',
+      component: Signup
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
     }
   ]
 })
